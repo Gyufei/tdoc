38987e6eb314b3ae090dd5ce72e3160ba89d7ddb
fei guo <812773673@qq.com>
2025-08-28 16:51:32 +0800

    fix:remove-unused-file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,22 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import { Redirect } from '@docusaurus/router';
 
-import styles from './index.module.css';
-
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary')}>
       <div className="container">
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-
-        </div>
+        <div></div>
       </div>
     </header>
   );
