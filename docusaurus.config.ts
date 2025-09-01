8b57bc1d02211a50f1c380204a19ad0de68911ad
fei guo <812773673@qq.com>
2025-09-01 18:16:57 +0800

    fix:replace-logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Tadle Docs',
   tagline: 'The Parallel Sandbox opening a new dimension of accessing dApps.',
-  favicon: 'img/favicon.ico',
+  favicon: 'https://cdn.tadle.com/icons/favicon-black.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -59,7 +59,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/social-card.png',
+    image: 'https://cdn.tadle.com/images/social-card.png',
     metadata: [
       { name: 'description', content: 'Tadle - The Parallel Sandbox opening a new dimension of accessing dApps.' },
 
@@ -67,22 +67,22 @@ const config: Config = {
       { property: 'og:title', content: 'Tadle Docs' },
       { property: 'og:description', content: 'Tadle - The Parallel Sandbox opening a new dimension of accessing dApps.' },
       { property: 'og:type', content: 'website' },
-      { property: 'og:image', content: 'https://v3.tadle.com/images/UjXLk9pSW552Wq3jVMIQU.png' },
+      { property: 'og:image', content: 'https://cdn.tadle.com/images/social-card.png' },
       { property: 'og:url', content: 'https://docs.tadle.com' },
 
       // Twitter Card
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:title', content: 'Tadle Docs' },
       { name: 'twitter:description', content: 'Tadle - The Parallel Sandbox opening a new dimension of accessing dApps.' },
-      { name: 'twitter:image', content: 'https://v3.tadle.com/images/UjXLk9pSW552Wq3jVMIQU.png' },
+      { name: 'twitter:image', content: 'https://cdn.tadle.com/images/social-card.png' },
       { name: 'twitter:site', content: '@tadle_com' },
     ],
     navbar: {
       title: '',
       logo: {
         alt: 'Tadle Logo',
-        src: 'img/logo.svg',
-        srcDark: 'img/logo-dark.svg',
+        src: 'https://cdn.tadle.com/images/logo-black.svg',
+        srcDark: 'https://cdn.tadle.com/images/logo-white.svg',
       },
     },
     prism: {
